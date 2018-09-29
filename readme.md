7f5b5f18f67d34b90789a5e47fa8089ee45aa538
userzhangjinlong <825188432@qq.com>
2018-09-29 17:47:56 +0800

    博客api端文件首次提交

---
+++ readme.md
@@ -0,0 +1,2 @@
+##API - Blog
+    基于laravel的分离后端个人blog的api接口开发
---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zjl
+ * Date: 18-9-29
+ * Time: 下午5:15
+ */
+
+Route::get('/', 'IndexController@index');
