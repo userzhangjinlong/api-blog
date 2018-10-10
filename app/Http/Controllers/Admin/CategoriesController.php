d6cfe798686b7e7c967d2828163920234dabeb20
userzhangjinlong <825188432@qq.com>
2018-10-10 17:08:58 +0800

    新增文章分类curd操作

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Controller;
 class CategoriesController extends Controller
 {
     /**
-     * 分类列表
+     * 分类列表（所有）
      * @param Category $category
      * @return \Illuminate\Http\JsonResponse
      */
@@ -17,6 +17,40 @@ class CategoriesController extends Controller
         $categories = $category
             ->orderBy('created_at', 'desc')
             ->get();
+        $data['code'] = 200;
+        $data['msg']  = 'ok';
+        $data['list'] = $categories;
+        return response()->json($data);
+    }
+
+    /**
+     * 分类列表
+     *
+     * @param Category $category
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function lists(Category $category, Request $request){
+        // 关键字过滤
+        if($keyword = $request->keyword ?? ''){
+            $category = $category->where('name', 'like', "%{$keyword}%");
+        }
+
+
+
+        if($request->created_at){
+            if($request->created_at == 'desc'){
+                $category = $category->orderBy('created_at', 'desc');
+            }else{
+                $category = $category->orderBy('created_at', 'asc');
+            }
+        }
+        $categories = $category->paginate(20);
+        // 修正页码
+        /*if( $articles->count() < 1 && $articles->lastPage() > 1 ){
+            return redirect($articles->url($articles->lastPage()));
+        }*/
+
         $data['code'] = 200;
         $data['msg']  = 'ok';
         $data['list'] = $categories;
@@ -39,6 +73,60 @@ class CategoriesController extends Controller
 
         $data = [];
         if($category){
+            $data['code'] = 200;
+            $data['msg']  = 'ok';
+            $data['info'] = [];
+        }else{
+            $data['code'] = '400';
+            $data['msg']  = 'fail';
+            $data['info'] = [];
+        }
+        return response()->json($data);
+    }
+
+    public function update(Request $request){
+        if(Category::where('id','=', $request->id)->update(['name' => $request->name])){
+            $data['code'] = 200;
+            $data['msg']  = 'ok';
+            $data['info'] = [];
+        }else{
+            $data['code'] = '400';
+            $data['msg']  = 'fail';
+            $data['info'] = [];
+        }
+        return response()->json($data);
+    }
+
+    /**
+     * 删除分类
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function destroy(Request $request){
+//        $delete = Category::where('id','=',$request->id)->delete();
+//        $delete = $category->delete($request->id);
+        if(Category::where('id','=',$request->id)->delete()){
+            $data['code'] = 200;
+            $data['msg']  = 'ok';
+            $data['info'] = [];
+        }else{
+            $data['code'] = '400';
+            $data['msg']  = 'fail';
+            $data['info'] = [];
+        }
+        return response()->json($data);
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroyAll(Request $request){
+        if(Category::whereIn('id', $request->id)->delete()){
             $data['code'] = 200;
             $data['msg']  = 'ok';
             $data['info'] = [];
--- routes/admin.php
+++ routes/admin.php
@@ -12,8 +12,13 @@ Route::post('/login', 'UsersController@login');
 Route::get('/logins', 'UsersController@creates');
 Route::post('/categoryAdd', 'CategoriesController@create');
 Route::get('/categoryList', 'CategoriesController@index');
+Route::post('/categoryListSx', 'CategoriesController@lists');
+Route::post('/categorySave', 'CategoriesController@update');
+Route::post('/categoryDel', 'CategoriesController@destroy');
+Route::post('/categoryDelAll', 'CategoriesController@destroyAll');
 Route::get('/articleList', 'ArticlesController@index');
 Route::post('/articleAdd', 'ArticlesController@create');
+Route::post('/articleDel', 'ArticlesController@destroy');
 
 Route::middleware('auth:admin')->get('/user', function (\Illuminate\Http\Request $request) {
     echo $request->user();
