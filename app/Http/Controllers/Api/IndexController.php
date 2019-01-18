a87b705c0c6715f75c65071751d166a078c58ad4
userzhangjinlong <825188432@qq.com>
2019-01-18 16:38:07 +0800

    新增首頁文章列表訪問量

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Api\Index;
+
 use App\Models\Api\Webvolume;
 use App\Models\Article;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
 
 
 class IndexController extends Controller
@@ -29,25 +30,40 @@ class IndexController extends Controller
     }
 
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function addView(Request $request){
-        $redis = New \Redis();
         $nowDay = Carbon::today()->timestamp;
-        $where  = [
-            'view_at'  =>  $nowDay
-        ];
-        $data = Webvolume::where('view_at', $nowDay)->find();
-        dd($data.' '.$nowDay.' '.$request->getClientIp());
-        $ips = $redis->sMembers($nowDay.'ips');
-        if(!empty($ips)){
-            if(!$redis->sIsMember($nowDay.'ips', $request->getClientIp())){
-                $redis->sAdd($nowDay.'ips', $request->getClientIp());
 
+        $ips =  Redis::smembers($nowDay.'ips');
+        if(!empty($ips)){
+            if(!Redis::sisMember($nowDay.'ips', $request->getClientIp())){
+                Redis::sadd($nowDay.'ips', $request->getClientIp());
+                $this->addData($nowDay);
             }
         }else{
-            $redis->sAdd($nowDay.'ips', $request->getClientIp());
+            Redis::sadd($nowDay.'ips', $request->getClientIp());
+            Redis::expire($nowDay.'ips', 86400);
+            $this->addData($nowDay);
         }
+        return response()->json(['code' => 200, 'msg' => 'ok']);
+    }
 
-
+    /**
+     * @param $timestamp
+     */
+    public function addData($timestamp){
+        $data = Webvolume::where('view_at', $timestamp)->first();
+        if(!empty($data->id)){
+            Webvolume::where('id', $data->id)->increment('view_num');
+        }else{
+            Webvolume::create([
+                'view_at'       =>      $timestamp,
+                'view_num'      =>      1
+            ]);
+        }
 
     }
 
